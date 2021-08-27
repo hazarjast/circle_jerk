5d518008c02152c1d63e2bb05e352d0f0834e7da
hazarjast <30390295+hazarjast@users.noreply.github.com>
2021-08-27 17:49:39 -0400

    Don't try to lock cell after two failures
    Added logic to pause locking attempts of preferred cell after two subsequent failures until the next day so that internet connectivity is not continually disrupted during an extended cell outage.

--- scripts/cell_lock_failsafe.sh
+++ scripts/cell_lock_failsafe.sh
@@ -3,6 +3,7 @@
 ALLDEST="google.com cloudflare.com"
 LOCKCELL="1,[EARCFN],[PCID]"
 INTERVAL=900
+CELLFAIL=0
 LOG="/var/log/cell_lock_failsafe"
 
 # Added below 2 minute wait before first run for those calling the script from 'rc.local'
@@ -32,22 +33,28 @@ do
   
   if [ $CONNECTED -eq 1 ]
   then
-      echo  "$(date) -  Internet connectivity OK." >> $LOG	
-	  echo  "$(date) -  Checking if cell is locked..." >> $LOG
-	  if echo -ne "AT+QNWLOCK=\"common/4g\"\r\n" | microcom -X -t 1000 /dev/ttyUSB2 | grep -q '"common/4g",0'
-	  then
-	      echo  "$(date) -  Cell not locked. Setting lock to $LOCKCELL..." >> $LOG
-		  echo -ne "AT+QNWLOCK=\"common/4g\",$LOCKCELL\r\n" | microcom -X -t 1000 /dev/ttyUSB2 >/dev/null 2>/dev/null
-		  echo  "$(date) -  Cell locked, waiting 30 seconds for reconnect..." >> $LOG
-		  sleep 30
-	      pinger
-          if [ $CONNECTED -eq 1 ]
+      echo  "$(date) -  Internet connectivity OK." >> $LOG
+      if [ $CELLFAIL -eq 0 ]
+      then
+          echo  "$(date) -  Checking if cell is locked..." >> $LOG
+          if echo -ne "AT+QNWLOCK=\"common/4g\"\r\n" | microcom -X -t 1000 /dev/ttyUSB2 | grep -q '"common/4g",0'
           then
-              echo  "$(date) -  Internet reachable, cell lock successful!" >> $LOG
-          fi  
-      else
+	      echo  "$(date) -  Cell not locked. Setting lock to $LOCKCELL..." >> $LOG
+	      echo -ne "AT+QNWLOCK=\"common/4g\",$LOCKCELL\r\n" | microcom -X -t 1000 /dev/ttyUSB2 >/dev/null 2>/dev/null
+              echo  "$(date) -  Cell locked, waiting 30 seconds for reconnect..." >> $LOG
+	      sleep 30
+	      pinger
+              if [ $CONNECTED -eq 1 ]
+              then
+                  echo  "$(date) -  Internet reachable, cell lock successful!" >> $LOG
+		  CELLFAIL=0
+	      else
+	          CELLFAIL=`expr $CELLFAIL + 1`    
+              fi  
+          else
 	      echo  "$(date) -  Cell is already locked." >> $LOG
-	  fi	
+          fi  
+      fi  
   fi
 	
   if [ $CONNECTED -eq 0 ]
@@ -60,6 +67,14 @@ do
       if [ $CONNECTED -eq 1 ]
       then
           echo "$(date) -  Internet is accessible again!" >> $LOG
+	  if [ $CELLFAIL -ge 2 ]
+	  then
+              MIDNIGHT=`echo $(($(date -d 23:59:59 +%s) - $(date +%s) + 1))
+	      echo  "$(date) -  Locking to selected cell disrupted Internet connectivity twice in the last hour." >> $LOG
+	      echo  "$(date) -  Waiting until tomorrow before attempting to lock the preferred cell again." >> $LOG
+	      sleep $MIDNIGHT
+	      CELLFAIL=0
+	  fi
       else
           echo "$(date) -  Internet connectivity could not be restored!" >> $LOG
       fi
